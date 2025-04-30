98e6bc9f549173a7c02f85d7df4a6f1dd005b83a
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2025-04-30 14:36:04 +0700

    HW for Seminar2 is ok

---
+++ I год/Факультативы/2 Парадигмы программирования и языки парадигм/2 Структурное и процедурное программирование на практике/MultTable/MultTable/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace MultTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выбрала процедурное, так как считаю так удобнее, нагляднее и методы могут быть переиспользованы при необходимости
+            View();
+        }
+
+        static void View()
+        {
+            bool input = true;
+            Console.WriteLine("Введите цифру\число или 0 для выхода: ");
+            while (input)
+            {
+                if (Int32.TryParse(Console.ReadLine(), out int n))
+                    if (n != 0)
+                    {
+                        PrintResult(Mult(n));
+                        Console.WriteLine("***************************************");
+                    }
+                    else
+                    {
+                        Console.WriteLine("До свидания!");
+                        input = false;
+                    }
+
+                else
+                {
+                    Console.WriteLine("Вы ввели неверный формат данных: ");
+                }
+            }
+        }
+
+        static string[,] Mult(int n)
+        {
+            string[,] result = new string[n,n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    result[i,j] = (i + 1).ToString() + " * " + (j + 1).ToString() + " = " + ((i + 1) * (j + 1)).ToString();
+                }
+            }
+            return result;
+        }
+
+        static void PrintResult(string[,] result)
+        {
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Console.Write(result[j,i] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
